bf500d8751ab321083936c72bf73cdb976d589f6
abauer <abauer@jpiicardinals.org>
2013-08-28 13:20:17 -0500

    files to work with: started enemies

---
+++ src/Crawler.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+
+public class Crawler extends Entity{
+
+	public Crawler(Graphics g, StageManager sm,Stage s,int startPos){
+		super(g,sm,s,startPos);
+
+	}
+
+	public void drawEntity(){
+
+	}
+
+}
