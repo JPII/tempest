18b378a15bd5434b582120c306b8799ffcd8825d
abauer <abauer@jpiicardinals.org>
2013-09-13 09:48:38 -0500

    crawlers charge

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,15 +1,51 @@
 import java.awt.*;
 
 public class Crawler extends Enemy {
-
+	
+	int rotateCount=0;
+	
 	public Crawler(Graphics g, StageManager sm, Stage s, int startPos) {
 		super(g, sm, s, startPos);
+	}
+	
+	private void left() {
+		pos--;
+		while (pos <= 0)
+			pos += s.getNumPos();
+		pos %= s.getNumPos();
+	}
+
+	private void right() {
+		pos++;
+		while (pos <= 0)
+			pos += s.getNumPos();
+		pos %= s.getNumPos();
+	}
+	
+	private void rotateTowardPlayer(){
+		if(sm.p.pos<pos)
+			left();
+		else
+			right();
 	}
 
 	public void drawEntity() {
 		if (z < s.getFrontZ())
-			z += 3;
+			z += 5;
+		else{
+			rotateCount++;
+			if(rotateCount>15){
+				rotateCount=0;
+				rotateTowardPlayer();
+			}
+		}
+		
+		Point3D one = s.getPos(pos);
+		Point3D two = s.getPos(pos + 1);
 
+		x = (one.getUserX() + two.getUserX()) / 2;
+		y = (one.getUserY() + two.getUserY()) / 2;
+		
 		Graphics3D temp = new Graphics3D(g);
 
 		for (double rad = 0; rad < Math.PI * 2; rad += 0.25) {
